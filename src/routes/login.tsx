abebf34b8c16144d9d4fb6f565e36003c90e6ed3
Saulo Roberto Pereira <srdevpereira@gmail.com>
2026-05-23 21:01:13 -0300

    fix: remove login auth from signup

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -73,7 +73,7 @@ function Login() {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: window.location.origin + "/dashboard",
+      redirectTo: "https://beta.txlogpay.com/dashboard",
     },
   });
 
